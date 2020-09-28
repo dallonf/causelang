a9f4a3b2a314d91026c295f4684100a98efaf4a8
Dallon Feldner <dallon.feldner@gmail.com>
2020-09-28 10:01:22 -0500

    Sample AST

--- proto_compiler/src/playground.ts
+++ proto_compiler/src/playground.ts
@@ -1,1 +1,129 @@
-console.log('hello world');
+const keywords = ["cause", "fn"] as const;
+const keywordSet = new Set(keywords);
+type KeywordLiteral = typeof keywords extends readonly (infer T)[] ? T : never;
+
+interface Keyword {
+  type: "Keyword";
+  keyword: KeywordLiteral;
+}
+
+interface Identifier {
+  type: "Identifier";
+  name: string;
+}
+
+interface StringLiteral {
+  type: "StringLiteral";
+  value: string;
+}
+
+interface IntLiteral {
+  type: "IntLiteral";
+  value: number;
+}
+
+type Literal = StringLiteral | IntLiteral;
+
+interface UnaryCallExpression {
+  type: "UnaryCallExpression";
+  callee: Expression;
+  argument: Expression;
+}
+
+interface CallExpression {
+  type: "CallExpression";
+  callee: Expression;
+  arguments: Expression[];
+}
+
+interface BlockExpression {
+  type: "BlockExpression";
+  body: Statement[];
+}
+
+type Expression =
+  | Keyword
+  | Identifier
+  | Literal
+  | UnaryCallExpression
+  | CallExpression
+  | BlockExpression;
+
+interface ExpressionStatement {
+  type: "ExpressionStatement";
+  expression: Expression;
+}
+
+type Statement = ExpressionStatement;
+
+interface FunctionDeclaration {
+  type: "FunctionDeclaration";
+  id: Identifier;
+  body: Expression;
+}
+
+type Declaration = FunctionDeclaration;
+
+interface Module {
+  type: "Module";
+  body: Declaration[];
+}
+
+const sampleAst: Module = {
+  type: "Module",
+  body: [
+    {
+      type: "FunctionDeclaration",
+      id: {
+        type: "Identifier",
+        name: "main",
+      },
+      body: {
+        type: "BlockExpression",
+        body: [
+          {
+            type: "ExpressionStatement",
+            expression: {
+              type: "UnaryCallExpression",
+              callee: {
+                type: "Keyword",
+                keyword: "cause",
+              },
+              argument: {
+                type: "CallExpression",
+                callee: {
+                  type: "Identifier",
+                  name: "Log",
+                },
+                arguments: [
+                  {
+                    type: "StringLiteral",
+                    value: "Hello World",
+                  },
+                ],
+              },
+            },
+          },
+          {
+            type: "ExpressionStatement",
+            expression: {
+              type: "CallExpression",
+              callee: {
+                type: "Identifier",
+                name: "ExitCode",
+              },
+              arguments: [
+                {
+                  type: "IntLiteral",
+                  value: 0,
+                },
+              ],
+            },
+          },
+        ],
+      },
+    },
+  ],
+};
+
+console.log("hello world");
