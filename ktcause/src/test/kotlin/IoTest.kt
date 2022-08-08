925d0c7eb331fe28b2a7b13a62591986dcc4d56a
Dallon Feldner <dallon.feldner@gmail.com>
2022-08-08 15:26:45 -0500

    Last working test from Rust version

---
+++ ktcause/src/test/kotlin/IoTest.kt
@@ -0,0 +1,98 @@
+import com.dallonf.ktcause.CompiledFile
+import com.dallonf.ktcause.Debug.debug
+import com.dallonf.ktcause.LangVm
+import com.dallonf.ktcause.RuntimeValue
+import com.dallonf.ktcause.types.CanonicalLangType
+import com.dallonf.ktcause.types.CanonicalLangTypeId
+import com.dallonf.ktcause.types.LangParameter
+import com.dallonf.ktcause.types.LangPrimitiveKind
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class IoTest {
+    fun ioFile(): CompiledFile {
+        val printId = CanonicalLangTypeId(
+            "test/io.cau", name = "Print", number = 0u
+        )
+        val promptId = CanonicalLangTypeId(
+            "test/io.cau", name = "Prompt", number = 0u
+        )
+
+        return CompiledFile("test/io.cau", types = buildMap {
+            put(
+                printId, CanonicalLangType.SignalCanonicalLangType(
+                    printId,
+                    "Print",
+                    params = listOf(LangParameter("message", LangPrimitiveKind.STRING.toValueLangType())),
+                    result = LangPrimitiveKind.ACTION.toValueLangType()
+                )
+            )
+            put(
+                promptId, CanonicalLangType.SignalCanonicalLangType(
+                    promptId, "Prompt", params = listOf(), result = LangPrimitiveKind.STRING.toValueLangType()
+                )
+            )
+        }, chunks = emptyList(), exports = buildMap {
+            put("Print", CompiledFile.CompiledExport.Type(printId))
+            put("Prompt", CompiledFile.CompiledExport.Type(promptId))
+        }, resolved = null)
+    }
+
+    @Test
+    fun receiveValueFromInputEffect() {
+        val vm = LangVm()
+        vm.addCompiledFile(ioFile())
+        TestUtils.addFileExpectingNoCompileErrors(
+            vm, "project/test.cau", """
+                import core/string ( append )
+                import test/io ( Print, Prompt )
+            
+                function main() {
+                    cause Print("What is your name?")
+                    cause Print(append("Hello, ", cause Prompt()))
+                }
+            """.trimIndent()
+        )
+
+        val result1 = TestUtils.expectValidCaused(
+            vm.executeFunction("project/test.cau", "main", listOf()),
+            vm.getTypeId("test/io.cau", "Print")
+        )
+        assertEquals(
+            result1.debug(), """
+            {
+                "#type": "test/io.cau:Print",
+                "message": "What is your name?"
+            }
+            """.trimIndent()
+        )
+
+        val result2 = TestUtils.expectValidCaused(
+            vm.resumeExecution(RuntimeValue.Action),
+            vm.getTypeId("test/io.cau", "Prompt")
+        )
+        assertEquals(
+            result2.debug(), """
+            {
+                "#type": "test/io.cau:Prompt"
+            }
+            """.trimIndent()
+        )
+
+        val result3 = TestUtils.expectValidCaused(
+            vm.resumeExecution(RuntimeValue.String("Bob")),
+            vm.getTypeId("test/io.cau", "Print")
+        )
+        assertEquals(
+            result3.debug(), """
+            {
+                "#type": "test/io.cau:Print",
+                "message": "Hello, Bob"
+            }
+            """.trimIndent()
+        )
+
+        val finalResult = vm.resumeExecution(RuntimeValue.Action).expectReturned()
+        assertEquals(finalResult.returnValue, RuntimeValue.Action)
+    }
+}
--- ktcause/src/test/kotlin/TestUtils.kt
+++ ktcause/src/test/kotlin/TestUtils.kt
@@ -1,6 +1,8 @@
 import com.dallonf.ktcause.LangVm
 import com.dallonf.ktcause.RunResult
 import com.dallonf.ktcause.RuntimeValue
+import com.dallonf.ktcause.types.CanonicalLangTypeId
+import kotlin.math.sign
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 
@@ -24,5 +26,11 @@ object TestUtils {
     fun expectInvalidSignal(result: RunResult): RuntimeValue.BadValue {
         require(result is RunResult.Caused)
         return result.signal.validate() as RuntimeValue.BadValue
+    }
+
+    fun expectValidCaused(result: RunResult, expectedType: CanonicalLangTypeId): RuntimeValue.RuntimeObject {
+        val signal = result.expectCaused().signal.validate() as RuntimeValue.RuntimeObject
+        assertEquals(signal.typeDescriptor.type.id, expectedType)
+        return signal
     }
 }
