825886c846916b079c39bb4f032141f0f1919b61
Dallon Feldner <dallon.feldner@gmail.com>
2022-08-09 15:46:13 -0500

    Failing test for first Effect implementation

---
+++ ktcause/src/test/kotlin/EffectsBasicTest.kt
@@ -0,0 +1,58 @@
+import com.dallonf.ktcause.CompiledFile
+import com.dallonf.ktcause.LangVm
+import com.dallonf.ktcause.RuntimeValue
+import com.dallonf.ktcause.types.CanonicalLangType
+import com.dallonf.ktcause.types.CanonicalLangTypeId
+import com.dallonf.ktcause.types.LangPrimitiveKind
+import org.junit.jupiter.api.Test
+import kotlin.test.assertEquals
+
+class EffectsBasicTest {
+    @Test
+    fun interceptSignal() {
+        val vm = LangVm()
+        val interceptThisTypeId = CanonicalLangTypeId("test/test.cau", name = "InterceptThis", number = 0u)
+        vm.addCompiledFile(
+            CompiledFile(
+                "test/test.cau",
+                types = mapOf(
+                    interceptThisTypeId to CanonicalLangType.SignalCanonicalLangType(
+                        interceptThisTypeId,
+                        interceptThisTypeId.name!!,
+                        listOf(),
+                        result = LangPrimitiveKind.ACTION.toValueLangType()
+                    )
+                ),
+                chunks = listOf(),
+                exports = mapOf(interceptThisTypeId.name!! to CompiledFile.CompiledExport.Type(interceptThisTypeId))
+            )
+        )
+        TestUtils.addFileExpectingNoCompileErrors(
+            vm, "project/test.cau", """
+                import test/test (InterceptThis)
+                
+                function main() {
+                    effect (_: InterceptThis) {
+                        cause Debug("Intercepted an InterceptThis effect")
+                    }
+                    
+                    cause InterceptThis()
+                    cause Debug("This should not have been intercepted")
+                    
+                }
+            """.trimIndent()
+        )
+
+        val debugTypeId = vm.getTypeId("core/builtin", "Debug")
+        val result1 = vm.executeFunction("project/test.cau", "main", listOf())
+            .let { TestUtils.expectValidCaused(it, debugTypeId) }
+        assertEquals(result1.values[0], RuntimeValue.String("Intercepted an InterceptThis effect"))
+
+        val result2 = vm.resumeExecution(RuntimeValue.Action)
+            .let { TestUtils.expectValidCaused(it, debugTypeId) }
+        assertEquals(result2.values[0], RuntimeValue.String("This should not have been intercepted"))
+
+        vm.resumeExecution(RuntimeValue.Action).expectReturnValue()
+            .let { assertEquals(it, RuntimeValue.Action) }
+    }
+}
