bcd35bf6828f4d335e9222b59a55db28b22ba4a0
Dallon Feldner <dallon.feldner@gmail.com>
2023-09-27 00:04:35 -0500

    Start resolving imports

--- rscause/rscause_compiler/src/resolve_types.rs
+++ rscause/rscause_compiler/src/resolve_types.rs
@@ -1,51 +1,76 @@
-use crate::ast;
-use crate::breadcrumbs::{Breadcrumbs, HasBreadcrumbs};
+use crate::ast::{self, BreadcrumbTreeNode};
+use crate::breadcrumbs::Breadcrumbs;
 use crate::lang_types::{InferredType, LangType};
+use crate::tags::NodeTag;
 use std::collections::HashMap;
+use std::sync::Arc;
+
+pub struct ExternalFileDescriptor {
+    pub exports: HashMap<Arc<String>, LangType>,
+}
+
+pub fn resolve_types(
+    file: &ast::FileNode,
+    node_tags: Arc<HashMap<Breadcrumbs, Vec<NodeTag>>>,
+    external_files: Arc<HashMap<Arc<String>, ExternalFileDescriptor>>,
+) -> ResolveTypesContext {
+    let mut ctx = ResolveTypesContext {
+        value_types: HashMap::new(),
+        node_tags,
+        external_files,
+    };
+    let descendants = BreadcrumbTreeNode::from(&Arc::new(file.clone())).descendants();
+    for descendant in &descendants {
+        descendant.resolve_types(&mut ctx);
+    }
+    ctx
+}
 
 pub struct ResolveTypesContext {
     value_types: HashMap<Breadcrumbs, InferredType<LangType>>,
-}
-
-pub fn resolve_types(file: &ast::FileNode) -> ResolveTypesContext {
-    let mut ctx = ResolveTypesContext {
-        value_types: HashMap::new(),
-    };
-    for declaration in &file.declarations {
-        declaration.resolve_types(&mut ctx);
-    }
-    ctx
+    node_tags: Arc<HashMap<Breadcrumbs, Vec<NodeTag>>>,
+    external_files: Arc<HashMap<Arc<String>, ExternalFileDescriptor>>,
 }
 
 trait ResolveTypes {
     fn resolve_types(&self, ctx: &mut ResolveTypesContext);
 }
 
-impl ResolveTypes for ast::DeclarationNode {
+impl ResolveTypes for ast::AnyAstNode {
     fn resolve_types(&self, ctx: &mut ResolveTypesContext) {
         match self {
-            ast::DeclarationNode::Import(node) => node.resolve_types(ctx),
-            ast::DeclarationNode::Function(node) => node.resolve_types(ctx),
-        }
-    }
-}
-
-impl ResolveTypes for ast::ImportNode {
-    fn resolve_types(&self, ctx: &mut ResolveTypesContext) {
-        for mapping in &self.mappings {
-            mapping.resolve_types(ctx);
+            Self::ImportMapping(node) => node.resolve_types(ctx),
+            _ => {}
         }
     }
 }
 
 impl ResolveTypes for ast::ImportMappingNode {
     fn resolve_types(&self, ctx: &mut ResolveTypesContext) {
-        todo!()
-    }
-}
-
-impl ResolveTypes for ast::FunctionNode {
-    fn resolve_types(&self, ctx: &mut ResolveTypesContext) {
-        todo!()
+        let tags = ctx.node_tags.get(&self.breadcrumbs);
+        let reference_file_tag = tags.and_then(|tags| {
+            tags.iter().find_map(|tag| match tag {
+                NodeTag::ReferencesFile(tag) => Some(tag.clone()),
+                _ => None,
+            })
+        });
+        let export_type = reference_file_tag.map(|tag| {
+            let file = ctx.external_files.get(&tag.path);
+            if let Some(file) = file {
+                if let Some(export) = file.exports.get(&self.source_name.text) {
+                    InferredType::Known(export.clone())
+                } else {
+                    println!("Export not found: {}::{}", tag.path, self.source_name.text);
+                    InferredType::Error
+                }
+            } else {
+                println!("File not found: {}", tag.path);
+                InferredType::Error
+            }
+        });
+        ctx.value_types.insert(
+            self.breadcrumbs.clone(),
+            export_type.unwrap_or(InferredType::Error),
+        );
     }
 }
