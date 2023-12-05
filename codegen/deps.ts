a5a14c1f1e7f7e7530e151ca507e0a5563f48d93
Dallon Feldner <dallonfdev@gmail.com>
2023-12-04 18:29:41 -0600

    Finally get proper watching in codegen

--- codegen/deps.ts
+++ codegen/deps.ts
@@ -1,5 +1,6 @@
 import handlebars from "npm:handlebars@^4.7.8";
 export * as changeCase from "npm:change-case@^4.1.2";
 export * as path from "https://deno.land/std@0.202.0/path/mod.ts";
+export * as fs from "https://deno.land/std@0.202.0/fs/mod.ts";
 
 export { handlebars };
--- codegen/generate.ts
+++ codegen/generate.ts
@@ -1,5 +1,24 @@
+import { fs, path } from "./deps.ts";
 import { generateAst } from "./src/ast/gen.ts";
 import { generateInstructions } from "./src/instructions/gen.ts";
 import { generateTags } from "./src/tags/gen.ts";
 
 await Promise.all([generateAst(), generateInstructions(), generateTags()]);
+
+const WATCH_DEBOUNCE = 300;
+
+const templateDirs = fs.walkSync("./src", {
+  match: [new RegExp(`\\${path.SEP}templates$`)],
+  includeDirs: true,
+  includeFiles: false,
+});
+const watcher = Deno.watchFs([...templateDirs].map((it) => it.path));
+console.log("watching for template changes...");
+let lastChange = Date.now();
+for await (const event of watcher) {
+  const currentChange = Date.now();
+  if (currentChange - lastChange < WATCH_DEBOUNCE) continue;
+  console.log(`${event.paths[0]} changed, regenerating...`);
+  lastChange = currentChange;
+  await Promise.all([generateAst(), generateInstructions(), generateTags()]);
+}
